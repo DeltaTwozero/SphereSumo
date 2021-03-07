3907017251975d8eaf33ee7242780643a19c600d
S.Prokopets <sprokopetss@gmail.com>
2021-03-06 21:57:26 -0500

    Menu updates
    Added placeholder for player sumo;
    Camera rotates around player's sumo;

--- SphereSumo/Assets/Scripts/MainCameraSpinFocus.cs
+++ SphereSumo/Assets/Scripts/MainCameraSpinFocus.cs
@@ -1,16 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainCameraSpinFocus : MonoBehaviour
 {
+    [SerializeField] Slider rotationSlider;
+
     [SerializeField] GameObject playerSphere;
+
+    bool isRotating;
+    float rotationSpeed;
     void Start()
     {
+        if(playerSphere != null)
+            {
+                isRotating = true;
+            }
+
+    }
+    private void Update()
+    {
+        if(rotationSlider != null)
+            {
+                rotationSpeed = rotationSlider.value;
+            }
+        else rotationSpeed = 2;
         
     }
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (isRotating)
+        {
+            this.transform.LookAt(playerSphere.transform.position);
+            this.transform.Translate(Vector3.left * Time.deltaTime * rotationSpeed);
+        }
     }
 }
