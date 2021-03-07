1bb8c3de0a6b644940d6384c251f4ec9793d3ee8
S.Prokopets <sprokopetss@gmail.com>
2021-03-06 21:39:20 -0500

    Added UI
    - Main menu

---
+++ SphereSumo/Assets/Scripts/MainCameraSpinFocus.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraSpinFocus : MonoBehaviour
+{
+    [SerializeField] GameObject playerSphere;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
---
+++ SphereSumo/Assets/Scripts/UIMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMenu : MonoBehaviour
+{
+    [SerializeField] GameObject MainMenuPanel; 
+    [SerializeField] GameObject HowToPanel; 
+    
+    public void PlayGame()
+        {
+            
+        }
+
+    public void HowToPlay()
+        {
+            MainMenuPanel.SetActive(false);
+            HowToPanel.SetActive(true);
+        }
+
+    public void BackToMain()
+        {
+            MainMenuPanel.SetActive(true);
+            HowToPanel.SetActive(false);
+        }
+}
